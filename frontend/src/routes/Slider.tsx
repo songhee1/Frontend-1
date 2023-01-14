b8faff35ca02786ec7dedeb44cd9c150a675d384
songhee1 <ghenrhkwk88@naver.com>
2023-01-14 22:46:14 +0900

    슬라이더 구현

--- frontend/src/routes/Slider.tsx
+++ frontend/src/routes/Slider.tsx
@@ -1,54 +1,53 @@
 import styled from 'styled-components';
-import { useState } from 'react';
-const Slider = () => {
-  const [moving, setMoving] = useState(0);
-  const SlideContainer = styled.div`
-    height: 300px;
-    width: 600px;
-    background-color: black;
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    overflow: hidden;
-  `;
-  const ImageSlider = styled.div`
-    left: ${moving}%;
-    width: 400%;
-    height: 100%;
-    position: absolute;
-    display: flex;
-    overflow: hidden;
-    transition: ${moving} 3s ease-in;
-  `;
-  const SliderElement = styled.div`
-    width: 100%;
-    background-color: grey;
-    height: 100%;
-    text-align: center;
-  `;
-  const Pointer = styled.div``;
-  const ChangePagesPoiner = styled.button`
-     ;
-  `;
-  const ChangePagesPoiner1 = styled.button`
-     ;
-  `;
+import { useEffect, useRef, useState } from 'react';
+import { AiFillLeftCircle, AiFillRightCircle } from 'react-icons/ai';
+import { BsFillCircleFill } from 'react-icons/bs';
 
+const Slider = () => {
+  const slideRef = useRef<any>(null);
+  const [slideNum, setSlideNum] = useState(0);
+  const slideRange = slideNum * -600;
+  const movingOne = () => {
+    setSlideNum((prev) => 0);
+  };
+  const movingTwo = () => {
+    setSlideNum((prev) => 1);
+  };
+  const movingThree = () => {
+    setSlideNum((prev) => 2);
+  };
+  const movingFour = () => {
+    setSlideNum((prev) => 3);
+  };
   const movingPrev = () => {
-    if (moving == 0) {
-      return;
-    }
-    setMoving((prev) => (prev += 100));
+    if (slideNum === 0) setSlideNum((prev) => prev + 4);
+    setSlideNum((prev) => prev - 1);
   };
   const movingNext = () => {
-    if (moving == -300) return;
-    setMoving((prev) => (prev -= 100));
+    if (slideNum === 3) setSlideNum((prev) => prev - 4);
+    setSlideNum((prev) => prev + 1);
   };
+  // const intervalId = setInterval(() => {
+  //   if (slideNum === 3) setSlideNum((prev) => prev - 3);
+  //   else setSlideNum((prev) => prev + 1);
+  // }, 3000);
+  useEffect(() => {
+    // const timeoutId = setTimeout(() => {
+    //   if (slideNum < 3) setSlideNum((prev) => prev + 1);
+    //   else setSlideNum(0);
+    // }, 4000);
+    slideRef.current != null
+      ? (slideRef.current.style.transform = `translate(${slideRange}px)`)
+      : null;
+    slideRef.current != null
+      ? (slideRef.current.style.transition = 'all 0.5s')
+      : null;
+    console.log(slideNum);
+  }, [slideNum]);
   return (
     <>
       <SlideContainer>
-        <ImageSlider>
+        <ImageSlider ref={slideRef}>
           <SliderElement>1</SliderElement>
           <SliderElement>2</SliderElement>
           <SliderElement>3</SliderElement>
@@ -56,10 +55,62 @@ const Slider = () => {
         </ImageSlider>
       </SlideContainer>
       <Pointer>
-        <ChangePagesPoiner onClick={movingPrev}>pointer1</ChangePagesPoiner>
-        <ChangePagesPoiner1 onClick={movingNext}>pointer2</ChangePagesPoiner1>
+        <ArrowPrevPoiner onClick={movingPrev}>
+          <AiFillLeftCircle size="36" />
+        </ArrowPrevPoiner>
+        <ArrowNextPoiner onClick={movingNext}>
+          <AiFillRightCircle size="36" />
+        </ArrowNextPoiner>
+        <ChangePagesPoiner onClick={movingOne}>
+          <BsFillCircleFill size="10" color={slideNum==0?"#dfe6e9":"black"}/>
+        </ChangePagesPoiner>
+        <ChangePagesPoiner onClick={movingTwo}>
+          <BsFillCircleFill size="10" color={slideNum==1?"#dfe6e9":"black"}/>
+        </ChangePagesPoiner>
+        <ChangePagesPoiner onClick={movingThree}>
+          <BsFillCircleFill size="10" color={slideNum==2?"#dfe6e9":"black"}/>
+        </ChangePagesPoiner>
+        <ChangePagesPoiner onClick={movingFour}>
+          <BsFillCircleFill size="10" color={slideNum==3?"#dfe6e9":"black"}/>
+        </ChangePagesPoiner>
       </Pointer>
     </>
   );
 };
 export default Slider;
+
+const SlideContainer = styled.div`
+  height: 300px;
+  width: 600px;
+  background-color: black;
+  overflow: hidden;
+  position: relative;
+`;
+const ImageSlider = styled.div`
+  width: 2400px;
+  height: 100%;
+  overflow: hidden;
+  display: flex;
+  flex-direction: row;
+  transform: translateX(0px);
+`;
+
+const SliderElement = styled.div`
+  width: 600px;
+  background-color: grey;
+  height: 100%;
+  text-align: center;
+`;
+const Pointer = styled.div`
+  display: flex;
+`;
+const ChangePagesPoiner = styled.div`
+  padding:0px 5px;
+`;
+const ArrowPrevPoiner = styled.div`
+  position: absolute;
+  transform: translate(-300px, -170px);
+`;
+const ArrowNextPoiner = styled(ArrowPrevPoiner)`
+  transform: translate(302px, -170px);
+`;
